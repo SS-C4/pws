5839b47bf30c315a174456e5dd95b1e819f51399
SS-C4 <srirams@iisc.ac.in>
2024-06-03 00:08:34 -0700

    update poso for sublinear verifier

--- poso.py
+++ poso.py
@@ -13,17 +13,22 @@
 # If all checks pass, output 1, else output 0
 
 import sys
+import math
 
 def print_inputs(vcount, num_poso, m, reps, f) -> int:
+    # Witness variables
     for i in range(num_poso):
         f.write("P V%d = I%d E\n" % (i, i))
         vcount += 1
+    # Randomness - m random variables per PoSO rep
     for i in range(m * reps):
         f.write("P V%d = I%d E\n" % (num_poso + i, num_poso + i))
         vcount += 1
-    for i in range(reps * n):
+    # Variables for bit decomposition - n bits per sum, ceil(num_poos/m) sums per rep
+    for i in range(reps * n * math.ceil(num_poso/m)):
         f.write("P V%d = I%d E\n" % (num_poso + m * reps + i, num_poso + m * reps + i))
         vcount += 1
+
     return vcount
 
 # print out an add tree given a list of inputs
@@ -44,6 +49,22 @@ def print_add_tree(fh, ivals, voffset, maxlength=1):
         return (ivals, voffset)
     else:
         return (ivals[0], voffset)
+    
+# print out an OR tree given a list of inputs
+def print_or_tree(fh, ivals, voffset):
+    while len(ivals) > 1:
+        outs = []
+        for idx in range(0, len(ivals) // 2):
+            fh.write("P V%d = V%d OR V%d E\n" % (voffset, ivals[2*idx], ivals[2*idx+1]))
+            outs.append(voffset)
+            voffset += 1
+
+        if len(ivals) % 2 != 0:
+            outs.append(ivals[-1])
+
+        ivals = outs
+
+    return (ivals[0], voffset)
 
 # sum a bit vector into a single field element
 def do_addition(fh, ivals, voffset, noconsts):
@@ -84,64 +105,101 @@ def print_poso(num_poso, n, m, reps, f):
     vcount = print_inputs(vcount, num_poso, m, reps, f)
 
     f.write("// PoSO\n")
-    sums = []
-    checks = []
+    sumchecks = []
     for i in range(reps):
         # PoSO sums
-        dp_vals = []
-        for j in range(num_poso):
-            dp_vals.append(vcount)
-            f.write("P V%d = V%d * V%d E\n" % (vcount, j, num_poso + i * num_poso + j))
-            vcount += 1
+        rep_sums = []
+        
+        for j in range(math.ceil(num_poso/m)):
+            elements_in_sum = []
+            for k in range(m):
+                idx = j * m + k
+                # This will add some random variables for the last PoSO but it's fine for benchmarking
+                elements_in_sum.append(idx)
 
-        while len(dp_vals) > 1:
-            outs = []
-            for i in range(0, len(dp_vals)//2):
-                f.write("P V%d = V%d + V%d E\n" % (vcount, dp_vals[2*i], dp_vals[2*i+1]))
-                outs.append(vcount)
+            # Multiply the PoSO variables by the random variables
+            dot_prod_vals = []
+            for k in range(m):
+                f.write("P V%d = V%d * V%d E\n" % (vcount, elements_in_sum[k], num_poso + i * m + k))
+                dot_prod_vals.append(vcount)
                 vcount += 1
 
-            if len(dp_vals) % 2 != 0:
-                outs.append(dp_vals[-1])
+            # Sum the dot_prod_vals
+            sum_var, vcount = print_add_tree(f, dot_prod_vals, vcount, 1)
+            rep_sums.append(sum_var)
 
-            dp_vals = outs
-        
-        sums.append(dp_vals[0])
-        
-        # Check if the sum is less than the PoSO bound
-        # The bits are already in the input, get the indices
-        bit_vals = []
-        for j in range(n):
-            bit_vals.append(num_poso + m * reps + i * n + j)
-        
-        # Recombine the bits
-        bitchecks, vcount = verify_bits(f, bit_vals, vcount)
-        checks.append(bitchecks)
-        
-        # Check if the recombined value is the same as the sum
-        recombined, vcount = do_addition(f, bit_vals, vcount, True)
+        # Recombine the bits and check if they're equal to the sums
+        for j in range(math.ceil(num_poso/m)):
+            bits_for_sum = []
+            for k in range(n):
+                bits_for_sum.append(num_poso + m * reps + i * n + j * n + k)
 
-        f.write("P V%d = V%d E\n" % (recombined, dp_vals[0]))
+            sumcheck, vcount = do_addition(f, bits_for_sum, vcount, True)
+            f.write("P V%d = V%d E\n" % (sumcheck, rep_sums[j]))
 
-    # OR of all checks should be 1
-    f.write("// Bit checks\n")
-    check, vcount = print_add_tree(f, [x[0] for x in checks], vcount, 1)
-    f.write("P V%d = V%d E\n" % (vcount, check))
+            sumchecks.append(sumcheck)
+
+    # OR of all sumchecks should be 0
+    f.write("// Sum checks\n")
+    check, vcount = print_or_tree(f, sumchecks, vcount)
 
     f.write("// Output\n")
-    for i in range(reps):
-        f.write("P V%d = O%d E\n" % (vcount, i))
-        vcount += 1
+    f.write("P V%d = O%d E\n" % (check, 0))
+
+
+    # for j in range(num_poso):
+    #     dp_vals.append(vcount)
+    #     f.write("P V%d = V%d * V%d E\n" % (vcount, j, num_poso + i * num_poso + j))
+    #     vcount += 1
+
+    # while len(dp_vals) > 1:
+    #     outs = []
+    #     for i in range(0, len(dp_vals)//2):
+    #         f.write("P V%d = V%d + V%d E\n" % (vcount, dp_vals[2*i], dp_vals[2*i+1]))
+    #         outs.append(vcount)
+    #         vcount += 1
+
+    #     if len(dp_vals) % 2 != 0:
+    #         outs.append(dp_vals[-1])
+
+    #     dp_vals = outs
+    
+    # sums.append(dp_vals[0])
+    
+    # # Check if the sum is less than the PoSO bound
+    # # The bits are already in the input, get the indices
+    # bit_vals = []
+    # for j in range(n):
+    #     bit_vals.append(num_poso + m * reps + i * n + j)
+    
+    # # Recombine the bits
+    # bitchecks, vcount = verify_bits(f, bit_vals, vcount)
+    # checks.append(bitchecks)
+    
+    # # Check if the recombined value is the same as the sum
+    # recombined, vcount = do_addition(f, bit_vals, vcount, True)
+
+    # f.write("P V%d = V%d E\n" % (recombined, dp_vals[0]))
+
+    # # OR of all checks should be 0
+    # f.write("// Bit checks\n")
+    # check, vcount = print_add_tree(f, [x[0] for x in checks], vcount, 1)
+    # f.write("P V%d = V%d E\n" % (vcount, check))
+
+    # f.write("// Output\n")
+    # for i in range(reps):
+    #     f.write("P V%d = O%d E\n" % (vcount, i))
+    #     vcount += 1
     
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print ("Usage: %s <num_poso> <n> <m> <reps>" % sys.argv[0])
         sys.exit(-1)
-    
+
     num_poso = int(sys.argv[1])
     n = int(sys.argv[2])
     m = int(sys.argv[3])
     reps = int(sys.argv[4])
 
-    with open("poso_%d_%d_%d_%d.pws" % (num_poso, n, m, reps), 'w') as f:
+    with open("../virgo-plus/data/poso_%d_%d_%d_%d.pws" % (num_poso, n, m, reps), 'w') as f:
         print_poso(num_poso, n, m, reps, f)
